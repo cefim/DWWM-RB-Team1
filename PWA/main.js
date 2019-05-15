d8f55b19664185dea5bcb436de570867ddb0b3e5
solenne <solenne2404@gmail.com>
2019-05-15 14:24:02 +0200

    ajout html/css/js

---
+++ PWA/main.js
@@ -0,0 +1,7 @@
+//INSPIRATION FROM: https://play.google.com/store/apps/details?id=com.omagrahari.abbeymusicplayernew&hl=en//
+
+function openNav() {    document.getElementById("mySidenav").style.width = "140px";  document.getElementById("main").style.marginLeft = "140px";
+}
+function closeNav() {    document.getElementById("mySidenav").style.width = "0";    document.getElementById("main").style.marginLeft= "0";
+}
+
