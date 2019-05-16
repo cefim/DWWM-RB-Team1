b4d6f129fde875477ae36536d41181c61d13a6a8
solenne <solenne2404@gmail.com>
2019-05-16 15:32:04 +0200

    js audio

---
+++ PWA/audio.js
@@ -0,0 +1,12 @@
+var player = document.querySelector('#audioPlayer');
+function play(idPlayer, control) {
+    var player = document.querySelector('#' + idPlayer);
+	
+    if (player.paused) {
+        player.play();
+        control.textContent = 'Pause';
+    } else {
+        player.pause();	
+        control.textContent = 'Play';
+    }
+}
--- PWA/main.js
+++ PWA/main.js
@@ -1,7 +1,9 @@
 //INSPIRATION FROM: https://play.google.com/store/apps/details?id=com.omagrahari.abbeymusicplayernew&hl=en//
 
-function openNav() {    document.getElementById("mySidenav").style.width = "140px";  document.getElementById("main").style.marginLeft = "140px";
+function openNav() {    document.getElementById("mySidenav").style.width = "140px";  
+document.getElementById("main").style.marginLeft = "140px";
 }
-function closeNav() {    document.getElementById("mySidenav").style.width = "0";    document.getElementById("main").style.marginLeft= "0";
+function closeNav() {    document.getElementById("mySidenav").style.width = "0";    
+document.getElementById("main").style.marginLeft= "0";
 }
 
